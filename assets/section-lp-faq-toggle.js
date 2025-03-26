e28dd64fa3689eca51736116e1848d785ae0c237
bestdream <motoyama999@outlook.com>
2025-03-26 07:44:50 -0700

    updated faq button toggle changed style and theme download

--- assets/section-lp-faq-toggle.js
+++ assets/section-lp-faq-toggle.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
   $(".question_contain").on("click", function() {
     $(this).siblings().slideToggle(300);
-    const specificChild = $(this).find(".faq_toggle");
-    if (specificChild.length) {
-      specificChild.toggleClass("anti_arrow");  
-    }
+    $(this).toggleClass("active");
   });
 });
